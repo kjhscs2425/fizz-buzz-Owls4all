a7f25470f077dca9c1fc72ceac8e00c57b88ae82
owls4all <owls4all@gmail.com>
2024-10-30 12:04:35 -0700

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz(fizz=3,buzz=5,duration=100):
+    for i in range(1,duration+1):
+        if i % fizz == 0:
+            if i % buzz == 0:
+                print("Fizz Buzz")
+            else:
+                print("Fizz")
+        elif i % buzz == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz(3,5)
